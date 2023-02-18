ed92efb1e0044660521e91ba20c0bb9ce8691fdd
Erik Langille <eriklangille@gmail.com>
2023-02-18 12:03:21 -0500

    Added automatic upload and some CSS fixes

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
+import argparse
 import shutil
+import time
 from dataclasses import dataclass
 from pathlib import Path
 
+import boto3
 import markdown
-from jinja2 import Template, FileSystemLoader, Environment, select_autoescape
+from jinja2 import FileSystemLoader, Environment, select_autoescape
+
+BUCKET_NAME = 'eriklangille.com'
+BUILD_FOLDER_PATH = Path('./build')
+DISTRIBUTION_ID = 'E200ARC50I19C0'
 
 @dataclass
 class Post:
@@ -57,9 +64,52 @@ def render_about():
 def copy_assets():
   shutil.copytree('assets', 'build/assets', dirs_exist_ok=True)
 
+def upload_to_s3(bucket_name: str, build_folder_path: Path):
+  s3 = boto3.client('s3')
+  
+  for path in build_folder_path.glob('**/*'):
+    if path.is_file():
+      s3_file_path = str(path.relative_to(build_folder_path))
+      if path.suffix == '.css':
+        s3.upload_file(str(path), bucket_name, s3_file_path, ExtraArgs={'ContentType': 'text/css'})
+      elif path.suffix == '.html':
+        s3.upload_file(str(path), bucket_name, s3_file_path, ExtraArgs={'ContentType': 'text/html'})
+      else:
+        s3.upload_file(str(path), bucket_name, s3_file_path)
+
+      print(f'Uploaded {path} to {s3_file_path}')
+
+def invalidate_cloudfront_distribution(distribution_id):
+  cloudfront = boto3.client('cloudfront')
+  
+  caller_reference = str(int(time.time()))
+  invalidation = cloudfront.create_invalidation(
+    DistributionId=distribution_id,
+    InvalidationBatch={
+      'Paths': {
+        'Quantity': 1,
+        'Items': ['/*']
+      },
+      'CallerReference': caller_reference
+    }
+  )
+  print(f"Invalidation created with ID: {invalidation['Invalidation']['Id']}")
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Builds the website')
+    parser.add_argument('-u', '--upload', action='store_true', help='Flag to indicate if upload to S3 should be performed')
+    args = parser.parse_args()
+    return args.upload
+
 if __name__ == '__main__':
+  upload = parse_args()
+
   posts = generate_posts()
   render_index(posts)
   render_blog_posts(posts)
   render_about()
   copy_assets()
+
+  if upload:
+    upload_to_s3(BUCKET_NAME, BUILD_FOLDER_PATH)
+    invalidate_cloudfront_distribution(DISTRIBUTION_ID)
